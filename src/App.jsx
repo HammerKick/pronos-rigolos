4ce4153ec2238c9ea601558f1494ce310e06d7eb
Avélin ABABIBE ARAKAZA <abvelin@gmail.com>
2026-07-11 19:41:38 +0200

    adding route for matches

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./components/Home";
 import Navbar from "./navbar/Navbar";
 import Equipes from "./components/Equipes";
+import Matches from "./components/matches/matches";
 import InfosEquipes from "./components/InfosEquipes";
 import InfosMatches from "./components/matches/info-matches";
 import Joueur from "./components/Joueur";
@@ -15,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/teams" element={<Equipes />} />
+          <Route path="/matches" element={<Matches />} />
           <Route path="/team/:teamId" element={<InfosEquipes />} />
           <Route path="/match/:matchId" element={<InfosMatches />} />
           <Route path="/player/:playerId" element={<Joueur />} />
